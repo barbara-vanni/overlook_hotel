61af7c1f326196d7979cebcec5ff399e069a721d
walidwade13 <walid.sek@laposte.net>
2025-05-05 16:23:47 +0200

    tests pull request walid

--- overlook_hotel/src/main/java/com/projet/overlook_hotel/OverlookHotelApplication.java
+++ overlook_hotel/src/main/java/com/projet/overlook_hotel/OverlookHotelApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OverlookHotelApplication {
-
+	{for (int i = 1; i <= 5; i++) {
+		System.out.println("Compteur : " + i);
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(OverlookHotelApplication.class, args);
 	}
